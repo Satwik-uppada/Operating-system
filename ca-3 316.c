6af1cf86c090ae3a2ca17b559487d7c90e19d568
Satwik <92086645+Satwik-uppada@users.noreply.github.com>
2023-04-14 17:46:21 +0530

    Update ca-3 316.c

--- ca-3 316.c
+++ ca-3 316.c
@@ -1,132 +1,209 @@
-// C implementation of First - Fit algorithm
-#include<stdio.h>
-#include <stdlib.h>
-int allocated=0;
-int wasted=0;
-int externalFragmentation=0;
 
-// Function to allocate memory to
-// blocks as per First fit algorithm
-void firstFit(int blockSize[], int m, int processSize[], int n)
-{
+#include <stdio.h>
+#include <string.h>
 
-    int i, j;
-    int allocation[n];
-    int allocatedBlocks[m];
-    int numAllocatedBlocks = 0;
-    int numFreeBlocks = m;
-    for(i = 0; i < n; i++)
-    {
-        allocation[i] = -1;
+
+
+void firstFit(int process_size[], int n, int block_size[], int m);
+void display_Allocation_Table(int process_size[], int allocation[], int n) ;
+void display_Allocated_Blocks(int allocation[], int m) ; 
+int  calculate_Wasted_Blocks(int allocation[], int m);
+int display_Allocated_MemorySize(int process_size[], int n) ;
+void displayRemainingMemory(int block_size[], int m);
+float  calculate_Fragmentation(int block_size[], int m,int destArray[]);
+float calculate_Average_of_Fragmentation(int total_fragmentation, int n);
+
+
+
+int allocation[100]; // i set default allocation size as 100
+
+
+
+// main function
+int main() {
+
+    printf("*********** Welcome to First-Fit Memory Management Algorithm **********");
+    int n, m; // intializing n and m
+    printf("\nEnter the number of processes: ");
+    scanf("%d\n", &n); // Taking user input for no.of processes user want to add
+    int process_size[n]; // intitalizing process_size array with length n
+
+    // Taking user input of process sizes with n iterations
+    for (int i = 0; i < n; i++) {
+        printf("Enter the size of process %d: ", i+1);
+        scanf("%d", &process_size[i]);
     }
-    for(i = 0; i < m; i++)
-    {
-        allocatedBlocks[i] = 0;
-    }
-    for (i = 0; i < n; i++)
-    {
-        for (j = 0; j < m; j++)
-        {
-            if (allocatedBlocks[j] == 0 && blockSize[j] >= processSize[i])
-            {
-                allocation[i] = j;
-                allocated =allocated+processSize[i];
-                blockSize[j]=blockSize[j]-processSize[i];
-                if (blockSize[j] == 0)
-                {
-                    numAllocatedBlocks++;
-                    numFreeBlocks--;
-                    allocatedBlocks[j] = 1;
-                } 
-                break;
-            }
-        }
-    }
-    for (i = 0; i < m; i++)
-    {
-        if (allocatedBlocks[i] == 0)
-        {
-            wasted += blockSize[i];
-        }
+
+    printf("\n\nEnter the number of blocks: ");
+    scanf("%d\n", &m); // Taking input for no.of blocks user want to add
+    int block_size[m];  // intitalizing block_size array with length m
+
+     // Taking user input of block sizes with m iterations
+    for (int i = 0; i < m; i++) {
+        printf("Enter the size of block %d: ", i+1);
+        scanf("%d", &block_size[i]);
     }
     
-    externalFragmentation = blockSize[0];
-    for (i = 1; i < m; i++)
-    {
-        if (blockSize[i] > externalFragmentation)
-        {
-            externalFragmentation = blockSize[i];
-        }
+    // basic inputs for testing the code 
+
+    //  int process_size[] = {212, 426, 112, 496};  // Process sizes
+    // int block_size[] = {100, 500, 200, 300, 600};  // Block sizes
+    // int n = sizeof(process_size) / sizeof(process_size[0]);  // Number of processes
+    // int m = sizeof(block_size) / sizeof(block_size[0]);  // Number of blocks
+
+    int destArray[m]; // a temperary array for storing intial block sizes
+
+    // adding elements of block size array to destArray by iterating each element
+    for (int i = 0; i < m; i++) {
+       destArray[i] = block_size[i];
     }
     
-    printf("\nProcess No.\t\tProcess Size\t\t\tBlock no.\n");
-    for (int i = 0; i < n; i++)
-    {
-        printf(" %i\t\t\t", i+1);
-        printf("%i\t\t\t\t", processSize[i]);
-        if (allocation[i] != -1)
-            printf("%i", allocation[i] + 1);
-        else
-            printf("Not Allocated");
-        printf("\n");
-    }
-    printf("\nTotal memory allocated: %i", allocated);
-    printf("\nTotal memory wasted: %i", wasted);
-    printf("\nExternal fragmentation: %i", externalFragmentation);
-    printf("\nNumber of allocated blocks: %i", numAllocatedBlocks);
-    printf("\nNumber of free blocks: %i", numFreeBlocks);
+
+    printf("\n\n-------------------------------------Memory Management Algorithm----------------------------------------------");
+    
+    // memset is a library function in C programming language that sets a block of memory to a specific value.
+    // It takes three arguments - a pointer to the starting address of the memory to be filled, the value to be set,
+    // and the number of bytes to be filled with the value
+    memset(allocation, -1, sizeof(allocation));  // Initialize allocation array to -1
+
+    firstFit(process_size, n, block_size, m);  // Run First Fit algorithm
+
+    display_Allocation_Table(process_size, allocation, n); // display allocation table
+
+    display_Allocated_Blocks(allocation, m);  // Display number of allocated blocks
+
+    int wasted_blocks = calculate_Wasted_Blocks(allocation, m);  // Calculate wasted blocks
+    printf("Number of wasted blocks: %d\n", wasted_blocks);
+
+    int allocated_memory=display_Allocated_MemorySize(process_size, n);  // return total memory  allocated to blocks
+    printf("Allocated memory: %d\n",allocated_memory);
+    
+    displayRemainingMemory(block_size, m);      // display remaining memory after allocation of the processes
+
+    float total_fragmentation=calculate_Fragmentation(block_size,m,destArray);  // calculate fragmentation 
+    printf("Total Fragmentation: %.2f\n",total_fragmentation);
+    
+    
+    float avg_fragmentation = calculate_Average_of_Fragmentation(total_fragmentation, n);  // Calculate average fragmentation
+    printf("Average fragmentation: %.2f\n", avg_fragmentation);
+    
+    return 0;
 }
 
 
-// Driver code
-int main()
-{
-	int m; //number of blocks in the memory
-	int n; //number of processes in the input queue
-	int k1;
-	int k2;
-	printf("Enter no.of elements you want to enter in block size: ");
-	scanf("%d",&k1);
-	printf("Enter no.of elements you want to enter in process size: ");
-	scanf("%d",&k2);
-
-	if(k1<=0){
-		printf("Invalid input for k1\n");
-		exit(1);
-	}
-	if(k2<=0){
-		printf("Invalid input for k2\n");
-		exit(1);
-	}
-	int blockSize[k1];
-	int processSize[k2];
-	printf("Enter %d elements to add in block size array:\n", k1);
-    for(int i = 0; i < k1; i++) {
-        scanf("%d", &blockSize[i]); // Input each element and store in array
+//function implements the first-fit algorithm
+void firstFit(int process_size[], int n, int block_size[], int m){
+    memset(allocation, -1, sizeof(allocation)); // Initialize allocation array to -1
+  // initially assigning -1 to all allocation indexes
+  // means nothing is allocated currently
+  for(int i = 0; i < n; i++)
+  {
+	allocation[i] = -1;
+  }
+ 
+  // take each process one by one and find
+  // first block that can accomodate it
+  for (int i = 0; i < n; i++)
+  {
+      for (int j = 0; j <m; j++) { 
+          if (block_size[j] >= process_size[i])
+          {
+              // allocate block j to p[i] process
+              allocation[i] = j;
+              // Reduce size of block j as it has acc
+              block_size[j] -= process_size[i];
+              break;
+          }
+      }
+                                        
     }
-
-    printf("The array elements in block size are:\n");
-    for(int i = 0; i < k1; i++) {
-        printf("%d ", blockSize[i]); // Print each element of the array
-    }
-    printf("\n");
-    
-     printf("Enter %d elements to add in process size:\n", k2);
-    for(int i = 0; i < k2; i++) {
-        scanf("%d", &processSize[i]); // Input each element and store in array
-    }
-
-    printf("The array elements in process size are:\n");
-    for(int i = 0; i < k2; i++) {
-        printf("%d ", processSize[i]); // Print each element of the array
-    }
-    printf("\n");
-    
-	m = sizeof(blockSize) / sizeof(blockSize[0]);
-	n = sizeof(processSize) / sizeof(processSize[0]);
-
-
-	firstFit(blockSize, m, processSize, n);
-
-	return 0 ;
 }
+
+// displays the allocation table in a tabular form with columns 
+//for process number, process size, and allocated block number.
+void display_Allocation_Table(int process_size[], int allocation[], int n) {
+
+  printf("\n\n");
+printf("+--------------------------------------------------------------+\n");
+printf("|Process No.\t\tProcess Size\tAllocated Block no.    |\n");
+printf("+--------------------------------------------------------------+\n");
+for (int i = 0; i < n; i++)
+{
+    printf("| %i\t\t\t", i+1);
+    printf("%i\t\t\t", process_size[i]);
+    if (allocation[i] != -1)
+        printf("%i \t       |", allocation[i] + 1);
+    else
+        printf("Not Allocated  |");
+    printf("\n");
+    printf("+--------------------------------------------------------------+\n");
+}
+}
+
+
+//calculates and returns the total memory allocated to the processes.
+int display_Allocated_MemorySize(int process_size[], int n) {
+    int total_allocated_memory = 0;
+    for (int i = 0; i < n; i++) {
+        if (allocation[i] != -1) {  // If process is allocated
+            total_allocated_memory += process_size[i];
+        }
+    }
+   
+   return total_allocated_memory;
+}
+
+
+// calculates and displays the number of allocated blocks.
+void display_Allocated_Blocks(int allocation[], int m) {
+    int num_allocated = 0;
+    for (int i = 0; i < m; i++) {
+        if (allocation[i] != -1) {
+            num_allocated++;
+        }
+    }
+    printf("Allocated_blocks: %d\n",num_allocated);
+}
+
+
+// calculates and returns the number of wasted (unallocated) blocks after the allocation is done.
+int calculate_Wasted_Blocks(int allocation[], int m) {
+    int wasted_blocks = 0;
+    for (int i = 0; i < m; i++) {  // For each memory block
+        if (allocation[i] == -1) {  // If block is not allocated
+            wasted_blocks++;
+        }
+    }
+    return wasted_blocks;
+}
+
+// displays the remaining memory(blocksizes) after allocation for each block.
+void displayRemainingMemory(int block_size[], int m) {
+    printf("\nBlock No.    Remaining memory after allocation\n\n");
+    for (int i = 0; i < m; i++) {
+        printf("   %d\t\t\t    %d\n", i+1, block_size[i]);
+    }
+    
+}
+
+
+// calculates and returns the total fragmentation of the blocks.
+float calculate_Fragmentation(int block_size[],int m,int destArray[]) {
+    int total_fragmentation = 0;
+      for(int i=0;i<m;i++){
+        if(block_size[i] != destArray[i]){
+             total_fragmentation += block_size[i];
+        }
+      }
+
+    return total_fragmentation;
+}
+
+
+// calculates and returns the average fragmentation of the blocks.
+float calculate_Average_of_Fragmentation(int total_fragmentation, int n) {
+    
+   return (float)total_fragmentation/n*100;
+}
+
+/*Average Fragementation= total fragmentation divided by no.of processes * 100 */
